a27c9cfb8f9176b1e936a3dc40b09b75fa56121b
Sanyam Jain <sanyamjain0315@gmail.com>
2024-02-17 13:42:11 +0530

    HI

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request, redirect
 from finance import search_youtube_videos
+import matplotlib.pyplot as plt
+import os
+import requests
+from PIL import Image
+from ocr import extract_receipt
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+db = client['budgetBuddy']
+collection = ['users']
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,9 +26,6 @@ def home():
 def calculator():
     return render_template('calculator.html')
 
-@app.route("/Scanner/")
-def Scanner():
-    return render_template('Scanner.html')
 
 @app.route("/videos/")
 def videos():
@@ -27,7 +36,58 @@ def videos():
 def Signup():
     return render_template('signup.html')
 
+@app.route("/Scanner/", methods=["GET", "POST"])
+def Scanner():
+    if request.method == "POST":
+        if request.files:
 
+            image = request.files['image']
+            image.save("static/sample.png")
+            try:
+                unique = extract_receipt()
+                return render_template('file_uploader.html', data=True, unique=unique, img=image)
+            except:
+                return render_template("file_uploader.html")
+
+
+
+
+            # return redirect(request.url)
+
+    return render_template('file_uploader.html', data=False)
 @app.route("/Signin/")
 def Signin():
     return render_template('signin.html')
+
+@app.route("/chatbot/", methods=['POST', 'GET'])
+def chatbot():
+    if request.method == "POST":
+        user_input = request.form['user_input']
+        API_URL = "http://localhost:3000/api/v1/prediction/3df2a231-e08b-4d2b-9583-4c4c6075e6d2"
+        payload = {"question": user_input}
+        response = requests.post(API_URL, json=payload).json()['text']
+        print(r"{}".format(response))
+        return render_template("chatbot.html", user_input=user_input, chatbot_response=response)
+    else:
+        return render_template("chatbot.html")
+
+
+@app.route("/update-balance", methods=["POST"])
+def getBalance():
+    if request.method == "POST":
+        global balance 
+        balance = int(request.form['new_balance'])
+        return render_template("balance_input.html")
+
+@app.route("/create-user/", methods=['GET', "POST"])
+def createUser():
+    if request.method == "POST":
+        username = request.form['username']
+        user = collection.find_one({"username": username})
+        if user:
+            password = request.form['password']
+            if password == user['password']:
+                return render_template("user_input.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
